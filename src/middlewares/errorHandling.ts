5b06a9f940d47d1cd90d04f726b0f8a3796e842a
Yasin Torun <yasintorun4680@gmail.com>
2023-02-18 20:48:42 +0300

    refactor: remove express and create cli

--- src/middlewares/errorHandling.ts
+++
@@ -1,8 +0,0 @@
-import { Response, Request, NextFunction } from 'express'
-
-export const errorHandlingMiddleware = (err: Error, req: Request, res: Response, next: NextFunction) => {
-    res.status(500).json({
-        status: false,
-        message: `An error occured. Error: ${err.message}`
-    })
-}
