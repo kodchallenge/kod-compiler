f4330c9a04f74e691f5b836fd195413173b5d3eb
Yasin Torun <yasintorun4680@gmail.com>
2023-02-18 21:12:31 +0300

    feat: created getOption

--- src/cli.ts
+++ src/cli.ts
@@ -1,3 +1,17 @@
 #!/usr/bin/env node
 
-console.log("Welcome to the CLI");
+// kcompiler --path=/usr/kc/problems/hello-world/solutions/usr-id/c
+
+console.log("Welcome to the CLI");  
+
+const args = process.argv.slice(2);
+
+const getOption = (optionName: string) => {
+    const option = args.find(arg => arg.startsWith(optionName));
+    if (!option) {
+        return null;
+    }
+    return option.split('=')[1];
+}
+
+console.table(getOption("--path"));
