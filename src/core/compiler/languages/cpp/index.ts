f7bc99dfed756c8e71cdebf5fa7fda51a8c18178
Yasin Torun <yasintorun4680@gmail.com>
2023-02-25 11:23:50 +0300

    fix: update path for cpp compiler

--- src/core/compiler/languages/cpp/index.ts
+++ src/core/compiler/languages/cpp/index.ts
@@ -1,11 +1,12 @@
 import { exec } from "child_process";
+import path from "path";
 import { ICompiler } from "../../interfaces/ICompiler";
 
 export class CppCompiler implements ICompiler {
     public execute(solutionPath: string): Promise<string> {
         return new Promise((resolve, reject) => {
-            const solutionFile = `${solutionPath}\\solution.cpp`;
-            const mainFile = `${solutionPath}\\main.cpp`;
+            const solutionFile = path.join(solutionPath, "solution.cpp");
+            const mainFile = path.join(solutionPath, "main.cpp");
             const command = `g++ ${mainFile} ${solutionFile} -o ${solutionPath}\\solution && ${solutionPath}\\solution`;
             exec(command, (err, stdout, stderr) => {
                 if (stderr) {
