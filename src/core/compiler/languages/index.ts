7566ed605c29bb60fa8cabb27a3224714ea15b67
Yasin Torun <yasintorun4680@gmail.com>
2023-02-26 16:15:39 +0300

    feat: created python compiler

--- src/core/compiler/languages/index.ts
+++ src/core/compiler/languages/index.ts
@@ -3,6 +3,7 @@ import { CCompiler } from "./c";
 import { CppCompiler } from "./cpp";
 import { JavaCompiler } from "./java";
 import { JavaScriptCompiler } from "./javascript";
+import { PythonCompiler } from "./python";
 import { TypeScriptCompiler } from "./typescript";
 
 const compilers: { [name: string]: ICompiler } = {
@@ -13,6 +14,8 @@ const compilers: { [name: string]: ICompiler } = {
     javascript: new JavaScriptCompiler(), // Maybe JS slug can come as javascript
     ts: new TypeScriptCompiler(),
     typescript: new TypeScriptCompiler(), // Maybe TS slug can come as typescript
+    py: new PythonCompiler(),
+    python: new PythonCompiler(), // Maybe Python slug can come as python
 }
 
 export const getCompiler = (language: string): ICompiler => {
