8554c928c8368bcd915a1cea4add40147a1e87f7
Yasin Torun <yasintorun4680@gmail.com>
2023-03-04 13:43:58 +0300

    feat: adde docker for javascript

--- README.md
+++ README.md
@@ -1,2 +1,20 @@
 # kod-compiler
  Kod Compiler project
+
+
+
+## with docker
+
+    Dockerfile
+    docker build -t app .
+    docker run -t -d app
+    docker ps
+    # burada container id olacak onu al
+    docker exec -it containerId sh -c "node /path/to/file.js"
+
+
+`Ana makinadan container a kopyalama`
+    docker cp foo.txt container_id:/foo.txt
+
+`Container dan ana makinaya kopyalama`
+    docker cp container_id:/foo.txt foo.txt
--- src/core/compiler/languages/javascript/index.ts
+++ src/core/compiler/languages/javascript/index.ts
@@ -1,19 +1,17 @@
-import { exec } from "child_process";
+import { exec, execSync } from "child_process";
 import path from "path";
 import { ICompiler } from "../../interfaces/ICompiler";
 
 export class JavaScriptCompiler implements ICompiler {
     execute(solutionPath: string): Promise<string> {
         return new Promise((resolve, reject) => {
-            // const solutionFile = path.join(solutionPath, "solution.js");
-            const mainFile = path.join(solutionPath, "main.js");
-            const command = `node ${mainFile}`;
-            exec(command, (err, stdout, stderr) => {
-                if (stderr) {
-                    return reject(stderr);
-                }
-                resolve(stdout);
-            });
+            execSync(`docker run -t -d nodeapp`);
+            const container = execSync("docker ps -l -q").toString().trim();
+            execSync(`docker cp ${solutionPath} ${container}:/app`);
+            const output = execSync(`docker exec ${container} /app/solution/main.js`).toString().trim();
+            execSync(`docker stop ${container}`);
+            execSync(`docker rm ${container}`);
+            resolve(output);
         });
     }
 }
