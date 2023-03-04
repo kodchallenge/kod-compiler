cdda17d5b151791c5abd47254d37184617007a8d
Yasin Torun <yasintorun4680@gmail.com>
2023-03-04 14:05:07 +0300

    feat: added type-catch for js command

--- src/core/compiler/languages/javascript/index.ts
+++ src/core/compiler/languages/javascript/index.ts
@@ -5,13 +5,18 @@ import { ICompiler } from "../../interfaces/ICompiler";
 export class JavaScriptCompiler implements ICompiler {
     execute(solutionPath: string): Promise<string> {
         return new Promise((resolve, reject) => {
-            execSync(`docker run -t -d nodeapp`);
-            const container = execSync("docker ps -l -q").toString().trim();
-            execSync(`docker cp ${solutionPath}/. ${container}:/app`);
-            const output = execSync(`docker exec -it ${container} sh -c "node /app/main.js"`).toString().trim();
-            execSync(`docker stop ${container}`);
-            execSync(`docker rm ${container}`);
-            resolve(output);
+            try {
+
+                execSync(`docker run -t -d nodeapp`);
+                const container = execSync("docker ps -l -q").toString().trim();
+                execSync(`docker cp ${solutionPath}/. ${container}:/app`);
+                const output = execSync(`docker exec -it ${container} sh -c "node /app/main.js"`).toString().trim();
+                execSync(`docker stop ${container}`);
+                execSync(`docker rm ${container}`);
+                resolve(output);
+            } catch(err: any) {
+                reject(err);
+            }
         });
     }
 }
